25deffdeecefc2c4e4da7848403578e0da6b1672
ForrestLin96 <63614850+ForrestLin96@users.noreply.github.com>
2020-06-13 01:55:22 -0400

    Add files via upload

--- SellPointAnalysis.py
+++ SellPointAnalysis.py
@@ -19,16 +19,12 @@ matplotlib.style.use('ggplot')
 from FunctionList import selljudge,stochastic_oscillator,calculate_k,calculate_dj,plot_precision_recall_vs_threshold
 
 method_name = [{
-                'Bayese-4':GaussianNB(var_smoothing=1e-04),
-                'Bayese-3':GaussianNB(var_smoothing=1e-03),
-                'Bayese-2':GaussianNB(var_smoothing=1e-02),
                 # 'SVC(C=1)':svm.SVC(probability=True),
                 # 'SVC(C=1.5)':svm.SVC(C=1.5,probability=True),
                 # 'SVC(linear, C=1)':svm.SVC(kernel='linear', C=1,probability=True),
-                # 'SVC(linear, C=1.5)':svm.SVC(kernel='linear',probability=True),
                 # 'SVC(poly, C=1)':svm.SVC(kernel='poly',probability=True),
-                'XGBT(λ=0.8)':Xgb(reg_lambda=0.8),
-                'XGBT(λ=1)':Xgb(reg_lambda=1),
+                # 'XGBT(λ=0.8)':Xgb(reg_lambda=0.8),
+                # 'XGBT(λ=1)':Xgb(reg_lambda=1),
                 }]
 method_list=pd.DataFrame(method_name)
 ResultTable=DataFrame(columns=['Stock','Method','AvgScores','StdScores'])
@@ -49,8 +45,8 @@ for stock in stocklist:
     df['SP500'] = df_SP500['Close']
     df['SP500_ROC'] = 100*df['SP500'].diff(1)/df['SP500'].shift(1)
     df['VIX'] = df_VIX['Close']
-    df['VIXMA3'] = df['VIX']/df['VIX'].rolling(3).mean()
-    df['VIXMA5'] = df['VIX']/df['VIX'].rolling(5).mean()
+    df['VIXMA3'] = df['VIX']/df['VIX'].rolling(5).mean()
+    df['VIXMA5'] = df['VIX']/df['VIX'].rolling(10).mean()
     df['VIX_ROC'] = 100*df['VIX'].diff(1)/df['VIX'].shift(1)
     df['Close_ROC'] = 100*df['Close'].diff(1)/df['Close'].shift(1)   
     stochastic_oscillator(df)
@@ -62,14 +58,18 @@ for stock in stocklist:
     df['Close/MA50']= df['Close']/df['Close'].rolling(50).mean()
     df['Close/MA100']= df['Close']/df['Close'].rolling(100).mean()
     df['Close/MA200']= df['Close']/df['Close'].rolling(200).mean()
+    df['VAR5']= df['Close_ROC'].rolling(5).std()
+    df['VAR10']= df['Close_ROC'].rolling(10).std()
     selljudge(df,loss=0.0149,cycle=10)
     df.dropna(axis=0, how='any', inplace=True)#Get rid of rows with NA value
     #Retrive X and y 
     X=df.loc[:,['# Inter 10-day','Intersection','MAVOL200','MAVOL20','MAVOL10','MAVOL5','SP500_ROC','VIX_ROC','VIXMA3','VIXMA5','Close_ROC','rsv','K','D','J',
                 'K_ROC','D_ROC','K_diff','D_diff','J_ROC','J_diff','Close/MA10','Close/MA20','Close/MA50',
-                'Close/MA100','Close/MA200',]] 
+                'Close/MA100','Close/MA200','VAR5','VAR10']] 
     ysell=df.loc[:,'Good Sell Point?']
-    
+    from sklearn import preprocessing   
+    min_max_scaler = preprocessing.MinMaxScaler()  
+    X = min_max_scaler.fit_transform(X) 
     # split train and test data
     xtrain,ytrain=X[:3500],ysell[:3500]
     xtest,ytest=X[3500:],ysell[3500:]
@@ -107,13 +107,13 @@ for method in method_list.loc[0,:]:
 
 #%%  Visualize the points       
 #clfsell = svm.SVC(C=1,probability=True)
-clfsell =Xgb(reg_lambda=0.8)
+clfsell =Xgb(reg_lambda=1)
 clfsell.fit(xtrain, ytrain)
 sellpredicted = clfsell.predict_proba(xtest)    
 dfplot=pd.DataFrame()
 dfplot.loc[:,'Close']=df[3500:]['Close']
 dfplot.loc[:,'GoodSellProb']=sellpredicted[:,1]
-for threshold in np.arange(0.85,0.9,0.01):
+for threshold in np.arange(0.87,0.92,0.01):
     dfplot['Sell']=0
     dfplot['SellPrice']=0
     dfplot.loc[(dfplot['GoodSellProb']>threshold),'Sell'] = 1
@@ -126,17 +126,17 @@ for threshold in np.arange(0.85,0.9,0.01):
     plt.plot(x, y2, 'o', ms=4.5, label='Sell Point',color='blue')
     plt.ylim([min(y1)-10, max(y1)+10])
     plt.title(stock+'\nXGBoost(λ=0.8)\nThreshold='+str(round(threshold,3)))
-    plt.figtext(0.35,0.1,'Sell Ratio='+str(Sellratio)+'%' , fontsize=13)
+    plt.figtext(0.35,0.2,'Sell Ratio='+str(Sellratio)+'%' , fontsize=13)
     plt.legend(loc='upper left')
     plt.show()
 #%%  Visualize the points       
-clfsell =Xgb(reg_lambda=0.8)
+clfsell =svm.SVC(kernel='poly',probability=True)
 clfsell.fit(xtrain, ytrain)
 sellpredicted = clfsell.predict_proba(xtest)    
 dfplot=pd.DataFrame()
 dfplot.loc[:,'Close']=df[3500:]['Close']
 dfplot.loc[:,'GoodSellProb']=sellpredicted[:,1]
-for threshold in np.arange(0.7,0.83,0.03):
+for threshold in np.arange(0.78,0.95,0.03):
     dfplot['Sell']=0
     dfplot['SellPrice']=0
     dfplot.loc[(dfplot['GoodSellProb']>threshold),'Sell'] = 1
@@ -148,7 +148,7 @@ for threshold in np.arange(0.7,0.83,0.03):
     plt.plot(x, y1,'c',label='Price')
     plt.plot(x, y2, 'o', ms=4.5, label='Sell Point',color='blue')
     plt.ylim([min(y1)-10, max(y1)+10])
-    plt.title(stock+'\nXGBoost(λ=0.6)\nThreshold='+str(round(threshold,3)))
+    plt.title(stock+'\nSVM(Poly))\nThreshold='+str(round(threshold,3)))
     plt.figtext(0.35,0.3,'Sell Ratio='+str(Sellratio)+'%' , fontsize=13)
     plt.legend(loc='upper left')
     plt.show()
